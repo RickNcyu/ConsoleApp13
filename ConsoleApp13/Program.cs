3c612a45f6321128f119337cf42eb1339eb31e8d
fzp <fzp@DESKTOP-HD39HVG>
2021-04-13 14:41:02 +0800

    加入專案檔案。

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using Npgsql;
+using System.Data;
+using System.IO;
+
+public class TSCLIB_DLL
+{
+    [DllImport("TSCLIB.dll", EntryPoint = "about")]
+    public static extern int about();
+
+    [DllImport("TSCLIB.dll", EntryPoint = "openport")]
+    public static extern int openport(string printername);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "barcode")]
+    public static extern int barcode(string x, string y, string type,
+                string height, string readable, string rotation,
+                string narrow, string wide, string code);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "clearbuffer")]
+    public static extern int clearbuffer();
+
+    [DllImport("TSCLIB.dll", EntryPoint = "closeport")]
+    public static extern int closeport();
+
+    [DllImport("TSCLIB.dll", EntryPoint = "downloadpcx")]
+    public static extern int downloadpcx(string filename, string image_name);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "formfeed")]
+    public static extern int formfeed();
+
+    [DllImport("TSCLIB.dll", EntryPoint = "nobackfeed")]
+    public static extern int nobackfeed();
+
+    [DllImport("TSCLIB.dll", EntryPoint = "printerfont")]
+    public static extern int printerfont(string x, string y, string fonttype,
+                    string rotation, string xmul, string ymul,
+                    string text);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "printlabel")]
+    public static extern int printlabel(string set, string copy);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "sendcommand")]
+    public static extern int sendcommand(string printercommand);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "setup")]
+    public static extern int setup(string width, string height,
+              string speed, string density,
+              string sensor, string vertical,
+              string offset);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "windowsfont")]
+    public static extern int windowsfont(int x, int y, int fontheight,
+                    int rotation, int fontstyle, int fontunderline,
+                    string szFaceName, string content);
+    [DllImport("TSCLIB.dll", EntryPoint = "windowsfontUnicode")]
+    public static extern int windowsfontUnicode(int x, int y, int fontheight,
+                     int rotation, int fontstyle, int fontunderline,
+                     string szFaceName, byte[] content);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "sendBinaryData")]
+    public static extern int sendBinaryData(byte[] content, int length);
+
+    [DllImport("TSCLIB.dll", EntryPoint = "usbportqueryprinter")]
+    public static extern byte usbportqueryprinter();
+}
+namespace ConsoleApp13
+{
+    class Program
+    {
+        
+       
+        //static string connectionString = @"Server=localhost;Database=postgres;User ID=postgres;Password=1234;";
+        static void Main(string[] args)
+        {
+            string fileName = @"D:/集點設定/集點.txt";
+            string fileName2 = @"D:/集點設定/金額.txt";
+            string temp = File.ReadLines(fileName, Encoding.Default).Last();
+            string[] arrTemp;
+            Console.WriteLine(temp);
+            arrTemp = temp.Split('值');
+            Console.WriteLine(arrTemp[1]);
+            string tempcost = File.ReadLines(fileName2, Encoding.Default).Last();
+            
+            //金額/集點設定值
+            /*int cost=Int32.Parse(tempcost);
+            int divide = Int32.Parse(arrTemp[1]);*/
+            int Point = Int32.Parse(tempcost) / Int32.Parse(arrTemp[1]);
+            Console.WriteLine(Point);
+                string WT1 = "Test Print";
+                string B1 = DateTime.Now.ToString("yyyy/MM/dd  HH:mm:ss", CultureInfo.InvariantCulture);
+                byte[] result_unicode = System.Text.Encoding.GetEncoding("utf-16").GetBytes("unicode test");
+                byte[] result_utf8 = System.Text.Encoding.UTF8.GetBytes("TEXT 40,620,\"ARIAL.TTF\",0,12,12,\"utf8 test Wörter auf Deutsch\"");
+                System.Diagnostics.Debug.WriteLine(B1);
+                //TSCLIB_DLL.about();
+                byte status = TSCLIB_DLL.usbportqueryprinter();//0 = idle, 1 = head open, 16 = pause, following <ESC>!? command of TSPL manual
+                TSCLIB_DLL.openport("TSC TTP-243E Pro");
+                TSCLIB_DLL.sendcommand("SIZE 40 mm, 20 mm");
+                TSCLIB_DLL.sendcommand("SPEED 4");
+                TSCLIB_DLL.sendcommand("DENSITY 12");
+                TSCLIB_DLL.sendcommand("DIRECTION 1");
+                TSCLIB_DLL.sendcommand("SET TEAR ON");
+                TSCLIB_DLL.sendcommand("CODEPAGE UTF-8");
+
+                TSCLIB_DLL.clearbuffer();
+                //TSCLIB_DLL.downloadpcx("UL.PCX", "UL.PCX");
+                TSCLIB_DLL.windowsfont(0, 0, 40, 0, 0, 0, "Arial", "東誠加油站 JiH002");
+                TSCLIB_DLL.windowsfont(10, 40, 30, 0, 0, 0, "Arial", DateTime.Now.ToString("yyyy/MM/dd  HH:mm:ss"));
+
+                //TSCLIB_DLL.windowsfontUnicode(40, 550, 48, 0, 0, 0, "Arial", result_unicode);
+                //TSCLIB_DLL.sendcommand("PUTPCX 40,40,\"UL.PCX\"");
+                //TSCLIB_DLL.sendBinaryData(result_utf8, result_utf8.Length);
+                TSCLIB_DLL.barcode("10", "70", "128", "35", "0", "0", "1", "1", B1);
+                TSCLIB_DLL.windowsfont(10, 110, 50, 0, 0, 0, "Arial", "點數"+Point.ToString()+"點");
+                //TSCLIB_DLL.printerfont("20", "40", "0", "0", "15", "15", WT1);
+                TSCLIB_DLL.printlabel("1", "1");
+                TSCLIB_DLL.closeport();
+               
+        }
+    }
+}
